a6915759fcf63a419842f91721dcc220f8211f9e
Haoyun Feng <haoyunfeng@Haoyuns-MacBook-Air.local>
2025-04-07 10:48:27 -0400

    Refine for open source

--- utils/paper_QA.py
+++ utils/paper_QA.py
@@ -45,29 +45,4 @@ class EmbeddingDB:
             print("-" * 80)
         return docs_with_score
 
-'''
-import google.generativeai as genai
-from google.api_core import retry
 
-genai.configure(api_key='<>')
-retry_policy = {"retry": retry.Retry(predicate=retry.if_transient_error)}
-class GeminiEmbeddingFunction:
-    # Specify whether to generate embeddings for documents, or queries
-    document_mode = True
-
-    def __call__(self, input):
-        if self.document_mode:
-            embedding_task = "retrieval_document"
-        else:
-            embedding_task = "retrieval_query"
-
-        retry_policy = {"retry": retry.Retry(predicate=retry.if_transient_error)}
-
-        response = genai.embed_content(
-            model="models/text-embedding-004",
-            content=input,
-            task_type=embedding_task,
-            request_options=retry_policy,
-        )
-        return response["embedding"]
-'''
